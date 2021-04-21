ab546cbf14e247f42df05755101af7f9520c7067
amiller <adam5bucks475@gmail.com>
2021-04-20 22:35:27 -0400

    First Git Commit!

---
+++ Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoccerTeamTracker.Models;
+
+namespace SoccerTeamTracker.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoccerTeamTracker.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Player> Players { get; set; }
+    }
+}
