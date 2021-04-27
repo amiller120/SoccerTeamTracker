12ce327ed33bfe87ae25fd13e780336643c0aeb1
amiller <adam5bucks475@gmail.com>
2021-04-26 21:46:52 -0400

    Added Seed data, nullable property.

--- Migrations/20210422004423_SeedTeamDataUpdatedPlayerAndTeamTables.Designer.cs
+++ Migrations/20210422004423_SeedTeamDataUpdatedPlayerAndTeamTables.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SoccerTeamTracker.Data;
@@ -9,9 +10,10 @@ using SoccerTeamTracker.Data;
 namespace SoccerTeamTracker.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210422004423_SeedTeamDataUpdatedPlayerAndTeamTables")]
+    partial class SeedTeamDataUpdatedPlayerAndTeamTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
